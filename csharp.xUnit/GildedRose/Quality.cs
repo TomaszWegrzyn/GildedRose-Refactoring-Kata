482795b2de70af350944cdd0bc2dacba6e4bec24
Wegrzyn, Tomasz (ext.) <tomasz.wegrzyn@proalpha.com>
2024-07-22 13:32:20 +0200

    Add Quality class

---
+++ csharp.xUnit/GildedRose/Quality.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace GildedRoseKata;
+
+public class Quality
+{
+    private readonly int _currentValue;
+    private readonly int _maxValue;
+
+    public Quality(int currentValue, int maxValue = int.MaxValue)
+    {
+        if (currentValue < 0)
+        {
+            throw new ArgumentException("Quality cannot be negative", nameof(currentValue));
+        }
+
+        if (maxValue < 1)
+        {
+            throw new ArgumentException("Quality max value must be positive", nameof(maxValue));
+        }
+
+        if (currentValue > maxValue)
+        {
+            throw new ArgumentException("Quality value cannot be above max value");
+        }
+        _currentValue = currentValue;
+        _maxValue = maxValue;
+    }
+
+    public Quality DecreaseBy(uint value)
+    {
+        // maybe catch overflow exceptions?
+        var potentialResult = _currentValue - (int)value;
+        var newValue = Math.Max(potentialResult, 0);
+        return new Quality(newValue, _maxValue);
+    }
+    
+    public Quality IncreaseBy(uint value)
+    {
+        // maybe catch overflow exceptions?
+        var potentialResult = _currentValue + (int)value;
+        var newValue = Math.Min(potentialResult, _maxValue);
+        return new Quality(newValue, _maxValue);
+
+    }
+
+    public static Quality operator +(Quality quality, uint value)
+    {
+        return quality.IncreaseBy(value);
+    }
+    
+    public static Quality operator -(Quality quality, uint value)
+    {
+        return quality.DecreaseBy(value);
+    }
+
+    public static explicit operator int(Quality quality)
+    {
+        return quality._currentValue;
+    }
+    
+    public static explicit operator Quality(int value)
+    {
+        return new Quality(value);
+    }
+}
---
+++ csharp.xUnit/GildedRoseTests/QualityTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests;
+
+public class QualityTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(44)]
+    [InlineData(111)]
+    [InlineData(123)]
+    [InlineData(1233)]
+    [InlineData(99999)]
+    [InlineData(int.MaxValue)]
+    public void CanCreateQualityWithPositiveOr0Value(int value)
+    {
+        _ = new Quality(value);
+    }
+    
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(2, 3)]
+    [InlineData(3, 6)]
+    [InlineData(44, 66)]
+    [InlineData(111, 188)]
+    [InlineData(123, 999)]
+    [InlineData(1233, 6666)]
+    [InlineData(99999, 99999)]
+    [InlineData(int.MaxValue, int.MaxValue)]
+    public void CanCreateQualityWithPositiveOr0ValueAndPositiveMaxValue(int value, int maxValue)
+    {
+        _ = new Quality(value, maxValue);
+    }
+    
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-3)]
+    [InlineData(-44)]
+    [InlineData(-111)]
+    [InlineData(-123)]
+    [InlineData(-1233)]
+    [InlineData(-99999)]
+    [InlineData(int.MinValue)]
+    public void QualityValueMustNotBeNegative(int value)
+    {
+        Assert.Throws<ArgumentException>(() => _ = new Quality(value));
+    }
+    
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(2, -1)]
+    [InlineData(3, -3)]
+    [InlineData(44, -234)]
+    [InlineData(111, -2342)]
+    [InlineData(123, -90000)]
+    [InlineData(1233, -100000)]
+    [InlineData(99999, int.MinValue)]
+    public void QualityMaxValueMustBePositive(int value, int maxValue)
+    {
+        Assert.Throws<ArgumentException>(() => _ = new Quality(value, maxValue));
+    }
+    
+    [Theory]
+    [InlineData(21, 1)]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(44, 34)]
+    [InlineData(111, 42)]
+    [InlineData(1001, 1000)]
+    [InlineData(1233, 1000)]
+    [InlineData(int.MaxValue, int.MaxValue - 1)]
+    public void CanNotCreateQualityWithValueAboveMaxValue(int value, int maxValue)
+    {
+        Assert.Throws<ArgumentException>(() => _ = new Quality(value, maxValue));
+    }
+
+    [Fact]
+    public void QualityHasExplicitConversionToInt()
+    {
+        var quality = new Quality(11);
+        _ = (int) quality;
+    }
+    
+    [Fact]
+    public void QualityHasExplicitConversionFromInt()
+    {
+        const int value = 42;
+        _ = (Quality) value;
+    }
+}
